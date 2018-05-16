8a99e78904c2ba080645d00fa34a68da75bf878a
Luís Falcão <lfalcao@cc.isel.pt>
2018-05-16 20:22:06 +0100

    Aula 2018-05-11: Using provided collectors from Collectors class to make queries over streams.

--- mpdLib/src/test/java/pt/isel/mpd/v1718/li41n/misc/streams/CollectorsTest.java
+++ mpdLib/src/test/java/pt/isel/mpd/v1718/li41n/misc/streams/CollectorsTest.java
@@ -4,14 +4,28 @@ package pt.isel.mpd.v1718.li41n.misc.streams;
 import org.junit.Test;
 import pt.isel.mpd.v1718.li41n.stream.ListCollector;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.IntStream;
 
 import static java.util.Arrays.asList;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.*;
 import static junit.framework.TestCase.assertEquals;
 
 public class CollectorsTest {
+
+    private String loremIpsum = "Lorem ipsum dolor sit amet consectetuer adipiscing elit. Nascetur sit lacus laoreet class metus sodales. Eni iaculis velit pretium et pretium fringilla gravida massa. In neque mollis placerat viverra. Iaculis tortor vestibulum vitae habitasse enim nam taciti hymenaeos condimentum ridiculus fusce eni nunc quam.\n" +
+            "\n" +
+            "Vel euismod taciti ut vitae duis nibh posuere molestie. Class malesuada elit. Consequat facilisi ornare cum. Commodo. Praesent. Aliquet dapibus diam fusce maecenas ve pellentesque class aliquam cum vivamus interdum. Scelerisque ultricies condimentum vitae. Luctus felis. Ante. Viverra platea adipiscing ultrices. Posuere sodales pede ut eget sapien ve enim at duis dictumst scelerisque tellus. Ipsum nulla penatibus nam velit vivamus sed. Fermentum vehicula fusce est natoque lacus tellus curabitur semper hymenaeos vel leo. Eu felis consectetuer parturient felis malesuada venenatis. Magna nisl sed dictum tristique non dictum. Platea orci primis leo habitasse lorem vitae penatibus fermentum libero.\n" +
+            "\n" +
+            "Fames leo litora venenatis gravida per id porta dictumst consectetuer leo. Eu suscipit maecenas orci cras aptent class. Scelerisque pretium fusce nunc ut mattis leo augue varius dignissim est ridiculus dui montes eni. Nostra mi tempus sociosqu auctor cubilia tempor ornare scelerisque cras. Ligula ve aliquam egestas montes vestibulum scelerisque platea ad lacinia netus parturient ornare luctus iaculis. Proin torquent integer natoque fames amet blandit est luctus pede fames parturient imperdiet. Odio hac nunc tellus mi pretium sollicitudin torquent eleifend commodo.\n" +
+            "\n" +
+            "Leo magnis enim rhoncus eni! Cursus nullam? Maecenas risus et odio elit hymenaeos ac ve taciti hymenaeos vestibulum adipiscing erat. Neque nostra eu torquent. Duis auctor id senectus blandit sodales taciti ac ac sit erat ad sollicitudin lobortis.\n" +
+            "\n" +
+            "Ve tellus mus adipiscing aliquet volutpat hymenaeos duis lorem urna aenean venenatis diam pellentesque sed cubilia. Morbi elit lorem. Hymenaeos donec erat dolor. Nulla elit primis phasellus ante commodo nisi tristique risus felis. Ad arcu tempus nonummy sit habitasse ac tristique tortor dictum lectus aptent! Fames ad nunc mauris placerat conubia adipiscing eget senectus potenti hendrerit congue magna rutrum penatibus. Senectus vivamus tempor dis posuere. Convallis eni convallis. Aenean inceptos per lacus pretium proin felis feugiat eni potenti ipsum euismod ac egestas venenatis. Aliquam velit mi class tellus facilisi sodales potenti nullam vestibulum proin malesuada quis. Sem congue phasellus scelerisque malesuada massa per erat ante.";
+    private String WORD_SEPARATOR_REG_EXP =  "[,|(\\s+)|\\.|!|\\?]";
+
+    private final String[] words = loremIpsum.split(WORD_SEPARATOR_REG_EXP);
 
     @Test
     public void shouldCollectAStreamToAListWithACollector() {
@@ -86,6 +100,92 @@ public class CollectorsTest {
                 );
 
         assertEquals(MAX, res.size());
+    }
+
+    @Test
+    public void shouldReduceAStreamToAMap() {
+        final List<String> strings = asList("Sport", "Lisboa", "e", "Benfica", "e");
+
+        final Map<Integer, String> res = strings.stream().collect(
+                toMap(String::length, identity(), (s1, s2) -> s1 + s2, TreeMap::new)
+        );
+
+
+        assertEquals(strings.size(), res.keySet().size());
+        assertEquals(strings.size(), res.values().size());
+
+
+    }
+
+    @Test
+    public void shouldJoinElmentsinAStringWithJoiningCollector() {
+        final List<String> strings = asList("Sport", "Lisboa", "e", "Benfica", "e");
+
+        String res = strings.stream().collect(joining());
+
+        assertEquals("SportLisboaeBenficae", res);
+    }
+
+    @Test
+    public void shouldJoinElmentsinAStringWithJoiningCollectorWithCustomSeparator() {
+        final List<String> strings = asList("Sport", "Lisboa", "e", "Benfica", "e");
+
+        String res = strings.stream().collect(joining("##"));
+
+        assertEquals("Sport##Lisboa##e##Benfica#e", res);
+    }
+
+    @Test
+    public void shouldJoinElmentsinAStringWithJoiningCollectorWithCustomSeparatorPrefixAndSufix() {
+        final List<String> strings = asList("Sport", "Lisboa", "e", "Benfica", "e");
+
+        String res = strings.stream().collect(joining("#", "[", "]"));
+
+        assertEquals("[Sport#Lisboa#e#Benfica#e]", res);
+    }
+
+    @Test
+    public void shouldGroupByStringSize() {
+        // String -> Map<Integer, List<String>>>
+
+        Map<Integer, List<String>> res = Arrays.stream(loremIpsum.split(WORD_SEPARATOR_REG_EXP))
+                .collect(groupingBy(String::length));
+
+    }
+        @Test
+    public void shouldGroupByStringSizeAndCount() {
+        // String -> Map<Integer, List<String>>>
+
+        final String[] words = loremIpsum.split(WORD_SEPARATOR_REG_EXP);
+
+        Map<Integer, Long> res = Arrays.stream(words)
+                .collect(groupingBy(String::length, counting()));
+
+
+        String str = res.entrySet().stream().map(Object::toString).
+                collect(joining(","));
+        System.out.println(str);
+
+
+    }
+
+    @Test
+    public void shouldGroupByFirstCharAndThenByWordSizeAndCount() {
+        // String -> Map<Char, Map<Integer, Long>>>
+
+        Map<Character, Map<Character, Map<Integer, Long>>> res = Arrays.stream(words).collect(
+                groupingBy(
+                        s -> s.length() != 0 ? s.toLowerCase().charAt(0) : '#',
+                        groupingBy(
+                                s -> s.length() != 0 ? s.toLowerCase().charAt(1) : '#',
+                                groupingBy(String::length, counting())
+                        )
+                )
+        );
+
+
+        String str = res.entrySet().stream().map(Object::toString).collect(joining(","));
+        System.out.println(str);
     }
 }
 
