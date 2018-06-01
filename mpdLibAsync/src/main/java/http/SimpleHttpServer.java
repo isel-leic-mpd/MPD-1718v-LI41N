953367d02489db6390d1bf10e77fdafcdd62cef4
Luís Falcão <lfalcao@cc.isel.pt>
2018-06-01 18:45:20 +0100

    Aula 2018-05-30 - Simple Http server using the Vertex library.

---
+++ mpdLibAsync/src/main/java/http/SimpleHttpServer.java
@@ -0,0 +1,34 @@
+package http;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+import util.Logging;
+
+
+import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
+
+public class SimpleHttpServer {
+
+    private static final int PORT = 3000;
+
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
+
+        final HttpServer httpServer = vertx.createHttpServer();
+
+        httpServer.requestHandler(req -> {
+            Logging.log("Request received for url " + req.uri());
+
+            final HttpServerResponse response = req.response();
+            response.setStatusCode(201);
+            response.headers().add("Content-Type", "text/html");
+            response.end("SLB!");
+        } );
+
+        httpServer.listen(PORT);
+        Logging.log("Listening on port {0}", PORT);
+
+        System.out.println("Bye");
+    }
+}
